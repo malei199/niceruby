4175de959514d3e94456a061d17bcfbf6fcec452
potato <291430932@qq.com>
2017-08-09 13:51:43 +0800

    页面读取相册列表完成，第六章内容完成

---
+++ project_root/contents/home.js
@@ -0,0 +1,16 @@
+$(function () {
+    var tmpl = {};
+    var tdata = {};
+    var initPage = function () {
+        $.get('/templates/home.html', function (d) {
+            tmpl = d;
+        });
+        $.getJSON('/albums.json', function (d) {
+            $.extend(tdata, d.data);
+        });
+        $(document).ajaxStop(function () {
+            var renderedPage = Mustache.to_html(tmpl, tdata);
+            $('body').html(renderedPage);
+        });
+    }();
+});
