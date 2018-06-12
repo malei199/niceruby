f66b0c5dea00b9eb8384d6c81af344a524f9759f
potato <291430932@qq.com>
2018-06-12 15:41:10 +0800

    我的页面开发完成，icon大小需要调整，等设计

--- xiakexing/pages/my/my_index.js
+++ xiakexing/pages/my/my_index.js
@@ -7,37 +7,44 @@ Page({
     btnList: {
       data: [{
           text: '提交项目',
-          img: '',
+          img: '../../images/my_list_icon1.png',
+          class: 'icon1',
           state: 0
         },
         {
           text: '已提交的项目',
-          img: '',
+          img: '../../images/my_list_icon2.png',
+          class: 'icon2',
           state: 1
         },
         {
           text: '管理报备的客户',
-          img: '',
+          img: '../../images/my_list_icon3.png',
+          class: 'icon3',
           state: 2
         },
         {
           text: '我的收藏',
-          img: '',
+          img: '../../images/my_list_icon4.png',
+          class: 'icon4',
           state: 3
         },
         {
           text: '关于我们',
-          img: '',
+          img: '../../images/my_list_icon5.png',
+          class: 'icon5',
           state: 4
         },
         {
           text: '客服热线',
-          img: '',
+          img: '../../images/my_list_icon6.png',
+          class: 'icon6',
           state: 5
         },
         {
           text: '个人设置',
-          img: '',
+          img: '../../images/my_list_icon7.png',
+          class: 'icon7',
           state: 6
         },
       ]
